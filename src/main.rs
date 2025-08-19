0a40b3183a2961ea3bd78d90a2fa970185880198
Ourkid1 <147047055+Ourkid1@users.noreply.github.com>
2025-08-19 13:24:43 +0100

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 use blkrs::run_lsblk;
 use clap::ColorChoice;
 use clap::{Arg, Command};
+use serde_json;
 
 fn main() {
     let matches = Command::new("lsblk")
@@ -12,12 +13,12 @@ fn main() {
             Arg::new("device")
                 .help("Device to query")
                 .required(true)
-                .index(1)
+                .index(1),
         )
         .get_matches();
 
     if let Some(device) = matches.get_one::<String>("device") {
-        let output = serde_json::to_string(&run_lsblk(&device)).unwrap();
+        let output = serde_json::to_string(&run_lsblk(device)).unwrap();
         println!("{}", output);
     } else {
         println!("No device provided");
