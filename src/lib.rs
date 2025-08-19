dea51bd3b57c0c33c42d1fba7577c45d48a00dc3
Ourkid1 <147047055+Ourkid1@users.noreply.github.com>
2025-08-19 13:13:54 +0100

    Update lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -2,9 +2,7 @@ use std::process::Command;
 
 fn run_command(command: &str) -> String {
     let args: Vec<&str> = command.split(" ").collect();
-    let output = Command::new(args[0])
-        .args(&args[1..])
-        .output();
+    let output = Command::new(args[0]).args(&args[1..]).output();
     match output {
         Ok(output) => {
             let stdout = String::from_utf8_lossy(&output.stdout);
